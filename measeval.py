12b14e6be0b9da4186d49e73303023b96e8b7e62
Serena Cheng <serena.ch1999@gmail.com>
2020-11-18 04:07:32 -0500

    Get labels matched up between text and annot_df

--- measeval.py
+++ measeval.py
@@ -30,10 +30,57 @@ def get_text_data(text_files):
 
 
 def is_unit(token):
+    """Check if token is in units list"""
+
     if 'μ' in token.text:
         token.replace('μ', 'u')
 
     return token.lower_ in units or token.lemma_ in units
+
+
+def get_labels(doc_df, annot_df):
+    """Match up labels from annot_df to text and add to doc_df"""
+
+    labels = {'labels': []}
+
+    for _, t_row in doc_df.iterrows():
+        labeled_spans = [['O', (word.idx, (word.idx + len(word)))] for word in t_row['sent']]
+        match_doc_id = annot_df['docId'] == t_row['docId']
+
+        for _, a_row in annot_df[match_doc_id].iterrows():
+            if a_row['annotType'] == 'Qualifier':
+                continue
+
+            start, end = a_row['startOffset'], a_row['endOffset']
+            annot_type = a_row['annotType'].lower()
+
+            if annot_type == tags['QNT'].lower():
+                annot_type = 'QNT'
+            elif annot_type == tags['MSE'].lower():
+                annot_type = 'MSE'
+            elif annot_type == tags['MSP'].lower():
+                annot_type = 'MSP'
+
+            for i, item in enumerate(labeled_spans):
+                if item[0] != 'O':
+                    continue
+
+                span = (item[1][0], item[1][1])
+
+                if span[0] <= start < span[1] and span[0] < end <= span[1]:
+                    labeled_spans[i][0] = 'B-' + annot_type
+                    break
+                elif span[0] < end <= span[1]:
+                    labeled_spans[i][0] = 'I-' + annot_type
+                    break
+                elif span[0] <= start < span[1]:
+                    labeled_spans[i][0] = 'B-' + annot_type
+                elif start <= span[0] and end >= span[1]:
+                    labeled_spans[i][0] = 'I-' + annot_type
+
+        labels['labels'].extend([label for label, span in labeled_spans])
+
+    doc_df['labels'] = labels.values()
 
 
 def word2features(sent, i):
@@ -59,8 +106,8 @@ def word2features(sent, i):
         features.update({
             '-1:word.lower': prev_word.lower_,
             '-1:word.lemma': prev_word.lemma_,
-            '-1:word.istitle()': prev_word.is_title,
-            '-1:word.isupper()': prev_word.is_upper,
+            '-1:word.is_title': prev_word.is_title,
+            '-1:word.is_upper': prev_word.is_upper,
             '-1:word.is_digit': prev_word.is_digit,
             '-1:word.like_num': prev_word.like_num,
             '-1:word.is_unit': is_unit(prev_word),
@@ -75,8 +122,8 @@ def word2features(sent, i):
         features.update({
             '+1:word.lower': next_word.lower_,
             '+1:word.lemma': next_word.lemma_,
-            '+1:word.istitle()': next_word.is_title,
-            '+1:word.isupper()': next_word.is_upper,
+            '+1:word.is_title': next_word.is_title,
+            '+1:word.is_upper': next_word.is_upper,
             '+1:word.is_digit': next_word.is_digit,
             '+1:word.like_num': next_word.like_num,
             '+1:word.is_unit': is_unit(next_word),
@@ -90,15 +137,11 @@ def word2features(sent, i):
 
 
 def sent2features(sent):
+    """Generate features for each sentence"""
     return [word2features(sent, i) for i in range(len(sent))]
 
 
-def sent2labels(sent):
-    return [label for label in sent]
-
-
-pd.set_option('display.max_columns', 10)
-pd.set_option('display.width', 500)
+tags = {'QNT': 'Quantity', 'MSE': 'MeasuredEntity', 'MSP': 'MeasuredProperty'}
 
 nlp = spacy.load('en_core_web_sm')
 
@@ -110,6 +153,9 @@ for key, val in u.__dict__.items():
 
         if val.name not in units and val.name.lower() not in nlp.Defaults.stop_words:
             units.append(val.name.lower())
+
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.width', 500)
 
 # ------------ Getting data for train and test files ------------
 train_text_files = glob.glob('data/train/txt/*.txt')
@@ -124,5 +170,5 @@ test_text_files = glob.glob('data/test/txt/*.txt')
 test_annot_files = glob.glob('data/test/tsv/*.tsv')
 test_annot_df = pd.concat([pd.read_csv(a_file, sep='\t', header=0) for a_file in test_annot_files])
 
-sent2features(train_doc_df.iat[0, 1])
+get_labels(train_doc_df, train_annot_df)
 
